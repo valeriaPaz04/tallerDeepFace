7c43f91d165bd9cac0aa5a1a043774ae4c095764
Sant15123 <sansuadur2006@gmail.com>
2026-06-21 18:01:33 -0500

    Preparar app para despliegue en Render

--- app.py
+++ app.py
@@ -344,5 +344,6 @@ def analyze():
 
 
 if __name__ == "__main__":
-    print("App corriendo en http://localhost:5000")
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
